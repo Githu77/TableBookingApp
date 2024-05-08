fe832d6f35f08d0acd54f381cd2d7fca9ffbe733
Gilbert Oguta <gilbertoguta37@gmail.com>
2024-05-08 13:30:59 +0300

    my updates

---
+++ web_flask/static/script.js
@@ -0,0 +1,17 @@
+let hotels = [
+    {
+        name:"Mama Nilishe Kilimani",
+        about: "https://mama-nilishe-kilimani.tappi.ke/",
+        rating: 9
+    }
+]
+
+function searchHotel()
+{
+    let hotelName = document.getElementById("search").value
+}
+
+function displayHotel()
+{
+
+}
